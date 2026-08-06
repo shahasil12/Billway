62ac2e2fb2402fd6ebe0c055e0e420d9702f4e0d
shahasil12 <shahasil54@gmail.com>
2026-08-06 12:11:24 +0530

    Sync all latest changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,24 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+gradle.projectsEvaluated {
+    subprojects {
+        if (project.hasProperty("android")) {
+            val androidExt = project.extensions.findByName("android")
+            if (androidExt != null && androidExt is com.android.build.gradle.BaseExtension) {
+                if (androidExt.namespace == null) {
+                    var pkgName = project.group.toString()
+                    if (pkgName.isEmpty()) {
+                        pkgName = "com.example.${project.name.replace("-", "_")}"
+                    }
+                    androidExt.namespace = pkgName
+                }
+            }
+        }
+    }
+}
+
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
